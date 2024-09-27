6a254767d2588343697e647e5ad4f3280699082f
Deo silaen <deokeldisilaen@gmail.com>
2024-09-27 17:10:38 +0700

    update

---
+++ EduDev/Assets/Scripts/SceneGame.cs
@@ -0,0 +1,9 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneGame : MonoBehaviour
+{
+    public void SwitchScene(){
+         SceneManager.LoadScene("MainGame");
+    }
+}
